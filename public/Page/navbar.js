1123dc44f30774f456949995840395e211c82784
Merkury32 <pjozefiak32@gmail.com>
2020-10-21 20:06:36 +0200

    start working with navbar adapt to login/logout account

---
+++ public/Page/navbar.js
@@ -0,0 +1,9 @@
+// const accountNav = document.getElementById('accountNav');
+// const accountList = document.getElementById('accountList');
+// const homeRegisterBtn = document.getElementById('registerBtn');
+
+// document.getElementById('logoutBtn').onclick = () => {
+//     homeRegisterBtn.style.display = 'none';
+//     accountNav.style.display = 'none';
+//     accountList.style.display = 'none';
+// }
